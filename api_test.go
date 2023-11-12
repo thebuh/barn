6580ecd2e399a5168b1b221f9cea981d0d41f7be
buh <buh@thebuh.ru>
2023-11-12 12:37:58 +0300

    Add basic connected clients tests

--- api_test.go
+++ api_test.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestApiServer_ConnectClient(t *testing.T) {
+	ip := ClientIp("127.0.0.2")
+	var d = Device{
+		Id:               "",
+		Type:             "",
+		Index:            0,
+		ConnectedClients: nil,
+	}
+	d.ConnectClient(ip)
+	assert.Equal(t, true, d.IsConnected(ip), "they should be equal")
+	assert.Equal(t, false, d.IsConnected("127.0.0.1"), "they should be equal")
+}
+
+func TestApiServer_DisconnectClient(t *testing.T) {
+	ip := ClientIp("127.0.0.2")
+	var d = Device{
+		Id:               "",
+		Type:             "",
+		Index:            0,
+		ConnectedClients: nil,
+	}
+	d.ConnectClient(ip)
+	assert.Equal(t, true, d.IsConnected(ip), "they should be equal")
+	d.DisconnectClient(ip)
+	assert.Equal(t, false, d.IsConnected(ip), "they should be equal")
+}
